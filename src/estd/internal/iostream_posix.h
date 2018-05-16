6007ec04d459b6af5baccd06a2d2fc20269bdcb7
Malachi Burke <malachi.burke@gmail.com>
2018-05-16 00:38:10 -0700

    Yanking in more from util.embedded

---
+++ src/estd/internal/iostream_posix.h
@@ -0,0 +1,15 @@
+#pragma once
+
+extern "C"
+{
+    #include <unistd.h>
+    #include <stdio.h>
+}
+
+
+// TODO: try using these once compile errors settle down
+// gold: https://github.com/SuperHouse/esp-open-rtos/blob/8368929a6658082ebe5eee6926232986e7895f60/libc/xtensa-lx106-elf/include/reent.h
+namespace FactUtilEmbedded { namespace std
+{
+
+} }
