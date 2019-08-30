c245be2e0fe2ef45b66c6df78f47481b73fcb960
Malachi Burke <malachi.burke@gmail.com>
2019-08-30 12:41:29 -0700

    Activating 'type1' and 'type2' version of item

--- src/estd/exp/memory_pool_1.h
+++ src/estd/exp/memory_pool_1.h
@@ -16,32 +16,70 @@
 // even if it was impressively powerful
 namespace estd { namespace experimental1 {
 
-// No attempt to conform to 'allocator' pattern, do that with an owning class with a 'has a'
-// relationship
-// TODO: Consider passing in TItem so that we can do 'type 1' item
-template <class T, std::size_t N>
-struct memory_pool
+// FIX: Need explicit control over memory alignment *OR* comment as to why we don't
+template<class T>
+struct memory_pool_item
 {
-    // instance provider being first base class means:
-    // 1 - tracked data precedes control data
-    // 2 - it's presumed 'next' is memory aligned, but that might not always be true
-    // this is a 'type 2' item
-    struct item :
-            estd::experimental::raw_instance_provider<T>,
-            // FIX: Need explicit control over memory alignment *OR* comment as to why we don't
-            estd::experimental::forward_node_base_base<item*>
+    // Type 1 item node* comes first in memory
+    struct type_1 :
+            estd::experimental::forward_node_base_base<type_1*>,
+            estd::experimental::raw_instance_provider<T>
     {
-        typedef estd::experimental::forward_node_base_base<item*> node_base_type;
+        typedef estd::experimental::forward_node_base_base<type_1*> node_base_type;
 
         // DEBT: forward_node_base_base demands some kind of initializing constructor, so indulge it
         // with a NULLPTR.  Optimize this out, and don't leverage its "pre initialization" side effect
-        item() : node_base_type(NULLPTR) {}
+        type_1() : node_base_type(NULLPTR) {}
+
+        // DEBT: Traits is the paradigm for doing this, so put it in a traits mechanism
+        static type_1* get_item_from_tracked(T* tracked)
+        {
+            // NOTE: Due to https://software.intel.com/en-us/blogs/2015/04/20/null-pointer-dereferencing-causes-undefined-behavior
+            // we don't dereference a null to find offsets.  This means to be safe we'll need a runtime evaluation
+            // on a dummy stack variable
+            // Future optimization would be to invoke 'offsetof' when available, including making our own 'offsetof' in
+            // scenarios where we could be sure it would not be undefined
+            type_1 for_offset_only;
+            std::ptrdiff_t diff = for_offset_only.buf - (byte*)&for_offset_only;
+            byte* adjuster = (byte*)tracked;
+            adjuster -= diff;
+            return reinterpret_cast<type_1*>(adjuster);
+        }
     };
+
+    // Type 2 item inline data comes first in memory
+    // instance provider being first base class means:
+    // 1 - tracked data precedes control data
+    // 2 - it's presumed 'next' is memory aligned, but that might not always be true
+    struct type_2 :
+            estd::experimental::raw_instance_provider<T>,
+            estd::experimental::forward_node_base_base<type_2*>
+    {
+        typedef estd::experimental::forward_node_base_base<type_2*> node_base_type;
+
+        // DEBT: forward_node_base_base demands some kind of initializing constructor, so indulge it
+        // with a NULLPTR.  Optimize this out, and don't leverage its "pre initialization" side effect
+        type_2() : node_base_type(NULLPTR) {}
+
+        // DEBT: Traits is the paradigm for doing this, so put it in a traits mechanism
+        static type_2* get_item_from_tracked(T* tracked)
+        {
+            return reinterpret_cast<type_2*>(tracked);
+        }
+    };
+};
+
+// No attempt to conform to 'allocator' pattern, do that with an owning class with a 'has a'
+// relationship
+template <class T, std::size_t N, class TItem = typename memory_pool_item<T>::type_2>
+struct memory_pool
+{
+    typedef TItem item_type;
 
 private:
     // DEBT: forward_list has a traits/allocator empty struct gobbling up space
-    estd::intrusive_forward_list<item> free_nodes;
-    estd::array<item, N> pool;
+    estd::intrusive_forward_list<item_type> free_nodes;
+    estd::array<item_type, N> pool;
 
 public:
     memory_pool()
@@ -51,7 +89,7 @@ public:
         // DEBT: Use last/insert_after instead of this
         for(std::size_t j = 0; j < N - 1; j++)
         {
-            item& i = pool[j];
+            item_type& i = pool[j];
             i.next(&pool[j + 1]);
         }
 
@@ -59,25 +97,25 @@ public:
     }
 
     // internal allocate call
-    item& reserve()
+    item_type& reserve()
     {
-        item& front = free_nodes.front();
+        item_type& front = free_nodes.front();
         free_nodes.pop_front();
         return front;
     }
 
     // internal deallocate call
     // NOTE: behavior undefined if 'i' wasn't reserved from this memory pool in the first place
-    void release(item& i)
+    void release(item_type& i)
     {
         free_nodes.push_front(i);
     }
 
     // given a pointer to the 'tracked data', acquire the related 'item'
     // DEBT: Put this into traits
-    item* get_item_from_tracked(T* tracked) const
+    static item_type* get_item_from_tracked(T* tracked)
     {
-        return reinterpret_cast<item*>(tracked);
+        return item_type::get_item_from_tracked(tracked);
     }
 
     // internal call counting how many 'free_nodes' are left
--- test/catch/experimental-mempool1-test.cpp
+++ test/catch/experimental-mempool1-test.cpp
@@ -4,17 +4,20 @@
 
 using namespace estd::experimental1;
 
-TEST_CASE("experimental memory_pool_1 tests")
+template <class TItem>
+void basics_section()
 {
     SECTION("basics")
     {
         CONSTEXPR std::size_t N = 10;
-        typedef memory_pool<int, N> memory_pool_type;
+        typedef memory_pool<int, N, TItem> memory_pool_type;
+        typedef typename memory_pool_type::item_type memory_pool_item_type;
         memory_pool_type pool;
 
+        /*
         std::size_t sz = sizeof(pool);
         std::size_t sz_item = sizeof(memory_pool_type::item);
-
+        */
         // DEBT: Test accounting for platform-specific padding
         //REQUIRE(sz_item == sizeof(int) + sizeof(void*));
         // DEBT: forward_list has a lingering allocator/traits floating in it that isn't optimized out
@@ -26,7 +29,7 @@ TEST_CASE("experimental memory_pool_1 tests")
 
             REQUIRE(free_slots == N);
 
-            memory_pool_type::item& item = pool.reserve();
+            memory_pool_item_type& item = pool.reserve();
 
             REQUIRE(pool.free_slots() == N - 1);
 
@@ -38,7 +41,7 @@ TEST_CASE("experimental memory_pool_1 tests")
         {
             // NOTE: Was able to "reuse" item from earlier code at one point.  Unexpected from
             // a reference type
-            memory_pool_type::item& item = pool.reserve();
+            memory_pool_item_type& item = pool.reserve();
             int* value = &item.value();
 
             item.value() = N;
@@ -47,7 +50,7 @@ TEST_CASE("experimental memory_pool_1 tests")
 
             SECTION("get_item_from_tracked")
             {
-                memory_pool_type::item* retrieved_item = pool.get_item_from_tracked(value);
+                memory_pool_item_type* retrieved_item = pool.get_item_from_tracked(value);
 
                 REQUIRE(retrieved_item == &item);
             }
@@ -60,10 +63,10 @@ TEST_CASE("experimental memory_pool_1 tests")
 
             REQUIRE(free_slots == N);
 
-            memory_pool_type::item& item1 = pool.reserve();
-            memory_pool_type::item& item2 = pool.reserve();
-            memory_pool_type::item& item3 = pool.reserve();
-            memory_pool_type::item& item4 = pool.reserve();
+            memory_pool_item_type& item1 = pool.reserve();
+            memory_pool_item_type& item2 = pool.reserve();
+            memory_pool_item_type& item3 = pool.reserve();
+            memory_pool_item_type& item4 = pool.reserve();
 
             item4.value() = N;
 
@@ -73,7 +76,7 @@ TEST_CASE("experimental memory_pool_1 tests")
             pool.release(item2);
             pool.release(item3);
 
-            memory_pool_type::item& item5 = pool.reserve();
+            memory_pool_item_type& item5 = pool.reserve();
 
             REQUIRE(item4.value() == N);
 
@@ -83,4 +86,19 @@ TEST_CASE("experimental memory_pool_1 tests")
             REQUIRE(free_slots == N);
         }
     }
+}
+
+
+// catch.hpp segfaults if we don't use two different test cases.  I speculate this
+// is because two very different tests fall under the same "namespace" defined in
+// basics_section via SECTION, etc.
+
+TEST_CASE("experimental memory_pool_1 tests: type1")
+{
+    basics_section<memory_pool_item<int>::type_1>();
+}
+
+TEST_CASE("experimental memory_pool_1 tests: type2")
+{
+    basics_section<memory_pool_item<int>::type_2>();
 }
